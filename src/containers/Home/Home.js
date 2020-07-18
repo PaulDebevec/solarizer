2f9267e46b6f14a11324b597e407541086e7d6d4
Nick Taylor <nickstaylor@gmail.com>
2020-07-18 11:12:32 -0600

    Add inputs for Home component and bring in sun icon

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,23 +4,58 @@ import './Home.css';
 
 const Home = () => {
   const [ address, updateAddress ] = useState('')
+  const [ city, updateCity] = useState('')
+  const [ state, updateState ] = useState('')
+  const [ zipCode, updateZipCode] = useState('')
+  const [ error, udpateError] = useState('false')
+
+
   const handleSubmit = () => {
-    console.log(address);
+    if (!address){
+
+    }
   }
 
   return (
     <div className="home-container">
-      <section className="address-form">
-        <h3 className="form-title">Enter your address</h3>
+      <form className="address-form">
+        <p className="error-message">New user?</p>
+        <h3 className="form-title">Start Solarizing Now:</h3>
         <input
-            type="search"
+            type="text"
             name="address"
-            placeholder="Address"
+            placeholder="street address"
             value={address}
+            required
             onChange={e => updateAddress(e.target.value)}
           />
+          <input
+            type="text"
+            name="city"
+            placeholder="city"
+            value={city}
+            required
+            onChange={e => updateCity(e.target.value)}
+          />
+          <input
+            type="text"
+            name="state"
+            placeholder="state (ex: CO)"
+            value={state}
+            required
+            onChange={e => updateState(e.target.value)}
+          />
+          <input
+            type="number"
+            maxLength ="5"
+            name="zipCode"
+            placeholder="zip code"
+            value={zipCode}
+            required
+            onChange={e => updateZipCode(e.target.value)}
+          />
           <button onClick={handleSubmit}>Submit</button>
-      </section>
+      </form>
       <section className="ecotip">
 
       </section>
