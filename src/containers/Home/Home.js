3dc8811dc4284e9519c1201d0c48bad7d47cce0d
Nick Taylor <nickstaylor@gmail.com>
2020-07-20 17:51:57 -0600

    Removed "name" field in home component, added containers for styling labels and inputs

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import './Home.css';
 import * as actions from '../../actions';
 import { connect } from 'react-redux'
 import ListOfStates from './ListOfStates';
+import sun from '../../images/sun.svg'
 
 const Home = ({ setCurrentProfile }) => {
-  const [name, updateName] = useState('')
+  // const [name, updateName] = useState('')
   const [address, updateAddress] = useState('')
   const [city, updateCity] = useState('')
   const [state, updateState] = useState('')
@@ -19,7 +20,7 @@ const Home = ({ setCurrentProfile }) => {
     if (zipCode.length !== 5 || !state) {
       return updateError('Please fill all Inputs')
     }
-    const userProfile = { name, address, city, state, zipCode }
+    const userProfile = { address, city, state, zipCode }
     setCurrentProfile(userProfile);
     updateValidatedUser(true)
   }
@@ -28,48 +29,66 @@ const Home = ({ setCurrentProfile }) => {
     <>
       {validatedUser && <Redirect to="/configure" />}
       <div className="home-container">
-        <form className="address-form">
-          <p className="error-message">New user?</p>
+        <form className="address-form" onSubmit={(e) => handleSubmit(e)}>
+        <div className="header-title">
+          <h1 className="title">S</h1>
+          <img src={sun} alt="sun" />
+          <h1 className="title">larizer</h1>
+        </div>
           <h3 className="form-title">Start Solarizing Now:</h3>
           {error &&
             <p className="error">{error}</p>
           }
-          <input
+          {/* <input
             type="text"
             name="name"
             placeholder="name"
             value={name}
             required
             onChange={e => updateName(e.target.value)}
-          />
-          <input
-            type="text"
-            name="address"
-            placeholder="street address"
-            value={address}
-            required
-            onChange={e => updateAddress(e.target.value)}
-          />
-          <div className="city-state">
+          /> */}
+          <div className="home-form-inputs-section">
+          <div className="home-form-item">
+            <label>Street Address</label>
             <input
               type="text"
-              name="city"
-              placeholder="city"
-              value={city}
+              name="address"
+              value={address}
               required
-              onChange={e => updateCity(e.target.value)}
+              onChange={e => updateAddress(e.target.value)}
+              />
+            </div>
+
+            <div className="city-state">
+              <div className="home-form-item">
+              <div className="city-label">
+                <label>City</label>
+                <input
+                  id="city-input"
+                  type="text"
+                  name="city"
+                  value={city}
+                  required
+                  onChange={e => updateCity(e.target.value)}
+                />
+              </div>
+              </div>
+              <div id="list-of-states-component">
+                <ListOfStates onChange={(state) => updateState(state)} />
+              </div>
+            </div>
+          <div className="home-form-item">
+            <label>Zip Code</label>
+            <input
+              type="number" maxLength="5"
+              name="zipCode"
+              value={zipCode}
+              required
+              onChange={e => updateZipCode(e.target.value)}
             />
-            <ListOfStates onChange={(state) => updateState(state)} />
           </div>
-          <input
-            type="number" maxLength="5"
-            name="zipCode"
-            placeholder="zip code"
-            value={zipCode}
-            required
-            onChange={e => updateZipCode(e.target.value)}
-          />
-          <button onSubmit={(e) => handleSubmit(e)}>Submit</button>
+        </div>
+          <button className="home-btn" type="submit">Begin</button>
         </form>
         <section className="ecotip">
         </section>
