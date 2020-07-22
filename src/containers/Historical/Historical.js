042a30b1b0851669e18626c9762d84e5b5e21d3c
Jeremy Poulter <Jpoulter@gmail.com>
2020-07-22 10:30:24 -0600

    Comment out unused imports

--- src/containers/Historical/Historical.js
+++ src/containers/Historical/Historical.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Historical.css'
-import { connect } from 'react-redux'
-import * as actions from '../../actions'
+// import { connect } from 'react-redux'
+// import * as actions from '../../actions'
 import { Link } from 'react-router-dom'
 
 const Historical = () => {
