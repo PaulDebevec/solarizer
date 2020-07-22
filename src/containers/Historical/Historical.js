e2bf9fcb32fdb68ad49e8d1833d8b698405fa626
Jeremy Poulter <Jpoulter@gmail.com>
2020-07-21 21:05:49 -0600

    Move Historical directory and files into containers directory,  update imports that reference these files

---
+++ src/containers/Historical/Historical.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Historical.css'
+import { connect } from 'react-redux'
+import * as actions from '../../actions'
+
+const Historical = () => {
+  return (
+    <div>
+      Historical Page!
+    </div>
+  )
+}
+
+export default Historical;
+
