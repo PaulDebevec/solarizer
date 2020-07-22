31d705c20d591ff61275fbe71f24e61fa4636ec9
Jeremy Poulter <Jpoulter@gmail.com>
2020-07-21 21:11:17 -0600

    Add push attribute to Redirects.  Use auto-format to align indentation

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -20,21 +20,21 @@ const Home = ({ setCurrentProfile }) => {
     if (zipCode.length !== 5 || !state) {
       return updateError('Please fill all Inputs')
     }
-    const userProfile = { address, city, state, zipCode, validatedUser: true}
+    const userProfile = { address, city, state, zipCode, validatedUser: true }
     setCurrentProfile(userProfile);
     updateValidatedUser(true)
   }
 
   return (
     <>
-      {validatedUser && <Redirect to="/configure" />}
+      {validatedUser && <Redirect push to="/configure" />}
       <div className="home-container">
         <form className="address-form" onSubmit={(e) => handleSubmit(e)}>
-        <div className="header-title">
-          <h1 className="title">S</h1>
-          <img src={sun} alt="sun" />
-          <h1 className="title">larizer</h1>
-        </div>
+          <div className="header-title">
+            <h1 className="title">S</h1>
+            <img src={sun} alt="sun" />
+            <h1 className="title">larizer</h1>
+          </div>
           <h3 className="form-title">Start Solarizing Now:</h3>
           {error &&
             <p className="error">{error}</p>
@@ -48,50 +48,50 @@ const Home = ({ setCurrentProfile }) => {
             onChange={e => updateName(e.target.value)}
           /> */}
           <div className="home-form-inputs-section">
-          <div className="home-form-item">
-            <label>Street Address</label>
-            <input
-              type="text"
-              name="address"
-              value={address}
-              required
-              onChange={e => updateAddress(e.target.value)}
+            <div className="home-form-item">
+              <label>Street Address</label>
+              <input
+                type="text"
+                name="address"
+                value={address}
+                required
+                onChange={e => updateAddress(e.target.value)}
               />
             </div>
 
             <div className="city-state">
               <div className="home-form-item">
-              <div className="city-label">
-                <label>City</label>
-                <input
-                  id="city-input"
-                  type="text"
-                  name="city"
-                  value={city}
-                  required
-                  onChange={e => updateCity(e.target.value)}
-                />
-              </div>
+                <div className="city-label">
+                  <label>City</label>
+                  <input
+                    id="city-input"
+                    type="text"
+                    name="city"
+                    value={city}
+                    required
+                    onChange={e => updateCity(e.target.value)}
+                  />
+                </div>
               </div>
               <div id="list-of-states-component">
                 <ListOfStates state={state} onChange={(state) => updateState(state)} />
               </div>
             </div>
-          <div className="home-form-item">
-            <label>Zip Code</label>
-            <input
-              type="number" maxLength="5"
-              name="zipCode"
-              value={zipCode}
-              required
-              onChange={e => updateZipCode(e.target.value)}
-            />
+            <div className="home-form-item">
+              <label>Zip Code</label>
+              <input
+                type="number" maxLength="5"
+                name="zipCode"
+                value={zipCode}
+                required
+                onChange={e => updateZipCode(e.target.value)}
+              />
+            </div>
           </div>
-        </div>
           <button className="home-btn" type="submit">Begin</button>
-        <Link to="/solarizerfaq">
+          <Link to="/solarizerfaq">
             <p className="what-is-solarizer">What is Solarizer?</p>
-        </Link>
+          </Link>
         </form>
         <section className="ecotip">
         </section>
