6a2785db43fb2b6cc0cdff1b6240f766f14640b7
Alex Latham <alexlthm@gmail.com>
2020-07-23 14:59:46 -0600

    Test component with hardcoded ecotip for styling.

--- src/containers/Ecotip/Ecotip.js
+++ src/containers/Ecotip/Ecotip.js
@@ -16,11 +16,12 @@ const Ecotip = () => {
       .then(response => setEcotip(response.data.ecotip))
   }, []) // [] may be unnecessary to put in [ecotip, setEcotip] since it won't change on its own <- could also cause a stack overflow like an endless loop
 
+  // next <p>, replace content with {ecotip}
   return (
     <div className="ecotip-container">
       <div className="ecotip-card">
         <h3 className="ecotip-title">Ecotip</h3>
-        <p className="ecotip-content">{ecotip}</p>
+        <p className="ecotip-content">An apple a day keeps the doctor away. Studies show doctors hate apples, so you should always have a bushel on hand.</p>
       </div>
     </div>
   );
