76b08c7167e223ff1eb8103f85a404935f048de6
Alex Latham <alexlthm@gmail.com>
2020-07-22 18:52:45 -0600

    Structure ecotip component and arrange components within background-container

---
+++ src/containers/Ecotip/Ecotip.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Ecotip.css';
+
+
+const Ecotip = () => {
+
+  return (
+    <div className="ecotip-container">
+      <div className="ecotip-card">
+        <h3 className="ecotip-title">Ecotip</h3>
+        <p className="ecotip-content">An apple a day keeps the doctor away. Studies show doctors hate apples, so you should always have a bushel on hand.</p>
+      </div>
+    </div>
+  );
+
+}
+
+export default Ecotip;
