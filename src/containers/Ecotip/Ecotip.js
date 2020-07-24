f19a1581559545d3d52a61907cfca4cddd3abeef
Alex Latham <alexlthm@gmail.com>
2020-07-23 18:58:47 -0600

    Remove comments, add placeholder local url to fetch ecotip from a dummy server

--- src/containers/Ecotip/Ecotip.js
+++ src/containers/Ecotip/Ecotip.js
@@ -5,23 +5,21 @@ const Ecotip = () => {
   const [ecotip, setEcotip] = useState('');
 
   useEffect(() => {
-    fetch('endpoint_url') // replace with actual URL or environmental variable
+    fetch('http://localhost:3001/ecotip')
       .then(response => {
         if (!response.ok) {
           throw new Error('Failed to fetch')
         }
         return response.json()
       })
-      .then(data => console.log(data.ecotip)) // might hang here -- troubleshooting only
-      .then(response => setEcotip(response.data.ecotip))
-  }, []) // [] may be unnecessary to put in [ecotip, setEcotip] since it won't change on its own <- could also cause a stack overflow like an endless loop
+      .then(response => setEcotip(response.ecotip))
+  }, [])
 
-  // next <p>, replace content with {ecotip}
   return (
     <div className="ecotip-container">
       <div className="ecotip-card">
         <h3 className="ecotip-title">Ecotip</h3>
-        <p className="ecotip-content">An apple a day keeps the doctor away. Studies show doctors hate apples, so you should always have a bushel on hand.</p>
+        <p className="ecotip-content">{ecotip}</p>
       </div>
     </div>
   );
