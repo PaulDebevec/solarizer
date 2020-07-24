6f60476c4e9e217e5813bfc0012a6b18bfd8fb4d
Jeremy Poulter <Jpoulter@gmail.com>
2020-07-23 19:47:01 -0600

    Test out some alternate styling

--- src/containers/Charts/Chart.js
+++ src/containers/Charts/Chart.js
@@ -84,8 +84,8 @@ const Chart = ({ solarData }) => {
           {chartType === 'pie' &&
             <Pie
               data={data}
-              width={'50%'}
-              height={'50%'}
+              width={'100%'}
+              height={'100%'}
               options={{
                 maintainAspectRatio: false,
                 title: {
