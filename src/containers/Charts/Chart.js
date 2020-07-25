a7244f71c394b80cc2479d791573f1ee8a5b0eb6
Alex Latham <alexlthm@gmail.com>
2020-07-25 15:00:13 -0600

    Remove unused vars for CI

--- src/containers/Charts/Chart.js
+++ src/containers/Charts/Chart.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { Bar, Line, Pie } from 'react-chartjs-2'
-import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 // import * as actions from '../../actions';
 import './Chart.css'
