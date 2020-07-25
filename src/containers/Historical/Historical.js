d79a1acdb6e95e4e73eeefcf35f82c927ffae740
Alex Latham <alexlthm@gmail.com>
2020-07-25 12:43:16 -0600

    Make POST request to localhost backend in historical component, disable placeholder fetch in configure component

--- src/containers/Historical/Historical.js
+++ src/containers/Historical/Historical.js
@@ -1,13 +1,49 @@
 import React from 'react'
 import './Historical.css'
-// import { connect } from 'react-redux'
-// import * as actions from '../../actions'
+import { connect } from 'react-redux'
+import * as actions from '../../actions'
 import { Link } from 'react-router-dom'
 
-const Historical = () => {
+const Historical = ({ userProfile, userQuote, loadSolarData }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    const splitAddress = userProfile.address.split(' ')
+    splitAddress.push(userProfile.city, userProfile.state, userProfile.zipCode)
+
+    const joinedAddress = splitAddress.join('+')
+
+    const fetch_object = {
+      name: "example search",
+      address: joinedAddress,
+      system_capacity: userQuote.systemSize,
+      azimuth: userQuote.azimuth,
+      tilt: userQuote.tilt,
+      array_type: userQuote.arrayType,
+      module_type: userQuote.moduleType,
+      losses: userQuote.systemLosses
+    }
+
+    console.log(fetch_object)
+
+    fetch('http://localhost:5000/results', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          solarizer_parameters: fetch_object
+        })
+      })
+
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch')
+        }
+        return response.json()
+      })
+      // .then(data => console.log(data))
+      .then(data => loadSolarData(data))
   }
 
   return (
@@ -20,54 +56,54 @@ const Historical = () => {
             <div className='historical-inputs-column'>
               <div className='month-label-input'>
                 <label>January:</label>
-                <input type='number' required />
+                <input type='number'/>
               </div>
               <div className='month-label-input'>
                 <label>February:</label>
-                <input type='number' required />
+                <input type='number'/>
               </div>
               <div className='month-label-input'>
                 <label>March:</label>
-                <input type='number' required />
+                <input type='number'/>
               </div>
               <div className='month-label-input'>
                 <label>April:</label>
-                <input type='number' required />
+                <input type='number'/>
               </div>
               <div className='month-label-input'>
                 <label>May:</label>
-                <input type='number' required />
+                <input type='number'/>
               </div>
               <div className='month-label-input'>
                 <label>June:</label>
-                <input type='number' required />
+                <input type='number'/>
               </div>
             </div>
 
             <div className='historical-inputs-column'>
               <div className='month-label-input'>
                 <label>July:</label>
-                <input type='number' required />
+                <input type='number'/>
               </div>
               <div className='month-label-input'>
                 <label>August:</label>
-                <input type='number' required />
+                <input type='number'/>
               </div>
               <div className='month-label-input'>
                 <label>September:</label>
-                <input type='number' required />
+                <input type='number'/>
               </div>
               <div className='month-label-input'>
                 <label>October:</label>
-                <input type='number' required />
+                <input type='number'/>
               </div>
               <div className='month-label-input'>
                 <label>November:</label>
-                <input type='number' required />
+                <input type='number'/>
               </div>
               <div className='month-label-input'>
                 <label>December:</label>
-                <input type='number' required />
+                <input type='number'/>
               </div>
             </div>
           </div>
@@ -87,5 +123,17 @@ const Historical = () => {
   )
 }
 
-export default Historical;
+const mapStateToProps = (state) => ({
+  userProfile: state.userProfile,
+  userQuote: state.userQuote
+})
 
+const mapDispatchToProps = (dispatch) => ({
+  // userQuote: (quote) => dispatch(actions.userQuote(quote)),
+  // allUserQuotes: (userQuote) => dispatch(actions.allUserQuotes(userQuote)),
+  loadSolarData: (data) => dispatch(actions.loadSolarData(data))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Historical);
+
+// export default Historical;
