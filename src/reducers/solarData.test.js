8f7e659d2e2c3365de759d8c6c3fdc1ab40d1c11
Jeremy Poulter <Jpoulter@gmail.com>
2020-07-26 00:01:01 -0600

    Add tests for the currentProfileReducer and userQuoteReducer

--- src/reducers/solarData.test.js
+++ src/reducers/solarData.test.js
@@ -4,4 +4,9 @@ describe('solarData', () => {
   it('should return the initial state', () => {
 
   })
+
+  it('when receiving SET_SOLAR_DATA, should update state with the inputs and outputs passed into the reducer', () => {
+
+  })
+
 })
