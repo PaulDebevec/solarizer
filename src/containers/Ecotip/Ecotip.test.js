e2fe4930cf9a33100c73b6f5acd85e1a6c46bec7
Alex Latham <alexlthm@gmail.com>
2020-07-27 09:48:43 -0600

    Add ecotip test, make component function asynchronous

--- src/containers/Ecotip/Ecotip.test.js
+++ src/containers/Ecotip/Ecotip.test.js
@@ -1,3 +1,34 @@
-describe('example', () => {
-  it('should render the correct content', () => {})
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../../reducers/index';
+import Ecotip from './Ecotip';
+import { currentUserProfile } from '../../actions'
+
+const testStore = createStore(rootReducer);
+
+const homeTestWrapper = () => {
+  return render(
+    <Provider store={testStore}>
+      <BrowserRouter>
+        <Ecotip />
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+describe('Ecotip', () => {
+  it('should render the correct content', () => {
+    const { getByText, findByText, debug } = homeTestWrapper()
+
+    const ecotipTitle = getByText('Ecotip')
+    // const ecotipText = findByText('An apple a day keeps the doctor away. Studies show doctors hate apples, so you should always have a bushel on hand.')
+
+    expect(ecotipTitle).toBeInTheDocument()
+    // expect(ecotipText).toBeInTheDocument()
+  })
 })
