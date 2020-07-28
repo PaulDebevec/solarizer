fa8286212db808f5395e42000a36a6f964d8ab1e
Jeremy Poulter <Jpoulter@gmail.com>
2020-07-28 10:40:37 -0600

    Adjust the positioning of elements in results table.  Take out declarations that aren't being used

--- src/containers/Results/Results.js
+++ src/containers/Results/Results.js
@@ -9,7 +9,7 @@ const Results = ({ solarData }) => {
   const tableData = months.map((item, index) => {
     return (
       <tr key={item}>
-        <td>{item}</td>
+        <td className='text-left'>{item}</td>
         <td>{Number(solarData.solRadMonthly[index]).toFixed(2)}</td>
         <td>{Number(solarData.acMonthly[index]).toFixed(2)}</td>
         <td>{Number(solarData.savingsMonthly[index]).toFixed(2)}</td>
