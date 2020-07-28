00c1a2f7492a09e3574cea3b222f5ebe0f700748
Nick Taylor <nickstaylor@gmail.com>
2020-07-28 12:54:18 -0600

    Added Error component for user navigating to incorrect url.  User is directed back to address page on click of button.  Also added tests. Error tests pass.

---
+++ src/containers/Error/Error.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+import * as actions from '../../actions';
+
+
+const Error = ({clearAllData})  => {
+    return (
+        <div className="address-form">
+        <p>Oops! A Wrong Path was taken.</p>
+            <Link to='/' >
+                <button className='back-to-login-btn' onClick={clearAllData}>Go Back to Home Page</button>
+            </Link >
+        </div>
+    )
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  clearAllData: () => dispatch(actions.clearAllData()),
+})
+
+export default connect(null, mapDispatchToProps)(Error)
